b7afed5f7a985164311a5d8e14802d5fdc249d6b
Şahan Hasret <sahanhasretankara@gmail.com>
2020-03-20 02:47:25 +0300

    Create python_auto_library_installer.py

---
+++ python_auto_library_installer.py
@@ -0,0 +1,31 @@
+﻿import os
+paketler = ["django","kivy","facebook-sdk","google","pygame","pandas","numpy","schedule","opencv-python"]
+
+print("****************Oto Paket Yükleyiciye Hoşgeldiniz*********************")
+print("****************Paketleri Otomatik Kurmak İçin 1'e Basın**************")
+print("****************Paketleri El ile Kurmak İçin 2'ye Basın***************")
+secim= input("")
+
+if secim =="1":
+	print("**************************************************************")
+	print("*************AGARTHA CYBER WARRİOR LOJİSTİK*******************")
+	print("**************************************************************")
+	for i in paketler:
+		os.system("pip install "+i)
+
+		
+		print("*************************************")
+		print("*************************************")
+		print("********** "+ i +" Yüklendi**********") 
+		print("*************************************")
+		print("*************************************")
+	
+if secim == "2":
+	while True:
+		paket = input("Paket Adını Yazınız  ")
+		os.system("pip install "+paket)
+		print("ÇIKMAK İÇİN CTRL VE C TUŞLARINA AYNI ANDA BASIN")
+
+print("********************İşlem Tamamlandı**************************")
+print("*************AGARTHA CYBER WARRİOR LOJİSTİK*******************")
+print("**************************************************************")
